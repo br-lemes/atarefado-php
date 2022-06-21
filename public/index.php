e7dfc3da0862403afeba742544d96cd2931e215e
Breno Ramalho Lemes <breno@br-lemes.net>
2022-06-21 09:29:17 -0400

    cSpell em cada diretório

--- public/index.php
+++ public/index.php
@@ -12,7 +12,7 @@ ini_set('display_errors', 1);
 error_reporting(-1);
 
 // setlocale(LC_ALL, 'pt_BR', 'pt_BR.utf-8', 'pt_BR.utf-8', 'portuguese');
-date_default_timezone_set('America/Cuiaba'); // cSpell: disable-line
+date_default_timezone_set('America/Cuiaba');
 
 require __DIR__ . '/../src/app/app.php';
 
