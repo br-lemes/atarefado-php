427408fd0f4a848c599bceed7ea37cfe6fea1cea
Breno Ramalho Lemes <breno@br-lemes.net>
2022-06-26 22:51:24 -0400

    Teste no método OPTIONS e em rota não encontrada

--- tests/integration/IndexControllerTest.php
+++ tests/integration/IndexControllerTest.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Tests\integration;
 
 use Tests\Utils\WebTestCase;
+use Psr\Log\LoggerInterface;
 
 class IndexControllerTest extends WebTestCase
 {
@@ -12,10 +13,30 @@ class IndexControllerTest extends WebTestCase
     {
         $this->client->get('/');
         $data = $this->client->getBodyArray();
-
         $this->assertEquals(200, $this->client->response->getStatusCode());
         $this->assertEquals([
             'message' => 'Hello, World!',
+        ], $data);
+    }
+    public function testOptions()
+    {
+        $this->client->options('*');
+        $data = $this->client->getBodyArray();
+        $this->assertEquals(200, $this->client->response->getStatusCode());
+        $this->assertEquals(null, $data);
+    }
+    public function testNotFound()
+    {
+        $this->app->getContainer()->get(LoggerInterface::class)->setHandlers([]);
+        $this->client->get('/not-found');
+        $data = $this->client->getBodyArray();
+        $this->assertEquals(404, $this->client->response->getStatusCode());
+        unset($data['trace']);
+        $this->assertEquals([
+            'message' => 'Not found.',
+            'status' => 'error',
+            'code' => 404,
+            'class' => 'HttpNotFoundException',
         ], $data);
     }
 }
